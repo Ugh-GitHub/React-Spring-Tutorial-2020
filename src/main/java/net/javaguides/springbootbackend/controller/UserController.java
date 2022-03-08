db96c4302cf6a6a32ad5a0b5abb19a20b46de24a
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2022-03-07 19:27:31 -0600

    added react front end

--- src/main/java/net/javaguides/springbootbackend/controller/UserController.java
+++ src/main/java/net/javaguides/springbootbackend/controller/UserController.java
@@ -3,6 +3,7 @@ package net.javaguides.springbootbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import net.javaguides.springbootbackend.model.User;
 import net.javaguides.springbootbackend.repository.UserRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/")
 public class UserController {
     
     @Autowired
     private UserRepository UserRepository;
 
     @GetMapping("users")
-    public List<User> getUsers(){
+    public List < User > getUsers(){
         return this.UserRepository.findAll();
     }
 }
---
+++ src/main/js/react-frontend/src/App/App.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import UserComponent from '../UserComponent/UserComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserComponent/>
+    </div>
+  );
+}
+
+export default App;
